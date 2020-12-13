532567bb8021c7ba28c52cf4c7b8c2c6160210c5
amardj <amar.jaiswal@ntnu.no>
2020-12-13 09:14:17 +0100

    dev checkin

--- src/main/java/no/ntnu/ism/lca/model/LcaVariables.java
+++ src/main/java/no/ntnu/ism/lca/model/LcaVariables.java
@@ -36,25 +36,25 @@ public class Patient {
     @ApiModelProperty(position = 5, required = true, value = "PainDuration range : [0, 10]", example = "2")
     int     painDuration = 2;
 
-    @ApiModelProperty(position = 6, required = true, value = "Sleep range : [0, 10]", example = "0")
+    @ApiModelProperty(position = 6, required = true, value = "Sleep range : [1, 5]", example = "0")
     int     sleep = 0;
 
-    @ApiModelProperty(position = 7, required = true, value = "Reverse Activity range : [0, 10]", example = "3")
+    @ApiModelProperty(position = 7, required = true, value = "Reverse Activity range : [1, 4]", example = "3")
     int     activity = 3;
 
     @ApiModelProperty(position = 8, required = true, value = "Pain Intensity Last Week range : [0, 10]", example = "4")
     int     painLastWeek = 4;
 
-    @ApiModelProperty(position = 9, required = true, value = "No. of Pain Sites range : [0, 12]", example = "1")
+    @ApiModelProperty(position = 9, required = true, value = "No. of Pain Sites range : [0, 10]", example = "1")
     int     painSiteCount = 1;
 
     @ApiModelProperty(position = 10, required = true, value = "Oreb Q10 range : [0, 10]", example = "5")
     int     orebQ7 = 5;
 
-    @ApiModelProperty(position = 11, required = true, value = "Reverse PSEQ range : [0, 10]", example = "0")
+    @ApiModelProperty(position = 11, required = true, value = "Reverse PSEQ range : [0, 12]", example = "0")
     int     pseq = 0;
 
-    @ApiModelProperty(position = 12, required = true, value = "Hscl Mean range : [0, 10]", example = "1.6")
+    @ApiModelProperty(position = 12, required = true, value = "Hscl Mean range : [0.0, 4.0]", example = "1.6")
     double  hsclMean = 1.6;
 
     @ApiModelProperty(position = 13, required = true, value = "Oreb Q10 range : [0, 10]", example = "5")
